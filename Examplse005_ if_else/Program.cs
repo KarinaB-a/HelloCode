009ce8de85214db892b006bf7ebbc31e552eb287
Karina Babakova <taytynovak@mail.ru>
2022-09-02 19:07:10 +0700

    Приветствие пользователя любимчика

---
+++ Examplse005_ if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "радость наша")
+{
+    Console.WriteLine("Ура, это же Радость наша! "); 
+}
+else
+{
+    Console.Write("Привет! ");
+    Console.WriteLine(username);
+}
